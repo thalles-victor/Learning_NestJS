bec1272403945032bff750468a3d89923164a26a
thalles-victor <thalles.victor2012@gmail.com>
2022-03-13 19:25:19 -0300

    empty

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
